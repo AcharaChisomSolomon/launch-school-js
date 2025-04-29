eac7dfa37bda17c27d81e0a950408701d1ce3c1b
AcharaChisomSolomon <ca0102c@gre.ac.uk>
2025-04-29 19:02:50 +0100

    1000 lights solved

--- courses/js101/lesson_2/calculator.js
+++ courses/js101/lesson_2/calculator.js
@@ -1,4 +1,5 @@
 const readline = require('readline-sync');
+const MESSAGES = require('./calculator_messages.json')['en']
 
 function prompt(message) {
   console.log(`=> ${message}`);
@@ -8,46 +9,80 @@ function invalidNumber(number) {
   return number.trimStart() === '' || Number.isNaN(Number(number));
 }
 
-prompt('Welcome to Calculator!');
+function getFirstNumber() {
+  prompt(MESSAGES['firstNumberRequest']);
+  let number1 = readline.question();
 
-prompt("What's the first number?");
-let number1 = readline.question();
+  while (invalidNumber(number1)) {
+    prompt(MESSAGES['invalidNumberError']);
+    number1 = readline.question();
+  }
 
-while (invalidNumber(number1)) {
-  prompt("Hmm... that doesn't look like a valid number.");
-  number1 = readline.question();
+  return number1
 }
 
-prompt("What's the second number?");
-let number2 = readline.question();
+function getSecondNumber() {
+  prompt(MESSAGES['secondNumberRequest']);
+  let number2 = readline.question();
 
-while (invalidNumber(number2)) {
-  prompt("Hmm... that doesn't look like a valid number.");
-  number2 = readline.question();
+  while (invalidNumber(number2)) {
+    prompt(MESSAGES['invalidNumberError']);
+    number2 = readline.question();
+  }
+
+  return number2
 }
 
-prompt('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
-let operation = readline.question();
+function getOperation() {
+  prompt(MESSAGES['operationRequest']);
+  let operation = readline.question();
 
-while (!['1', '2', '3', '4'].includes(operation)) {
-  prompt('Must choose 1, 2, 3 or 4')
-  operation = readline.question();
+  while (!['1', '2', '3', '4'].includes(operation)) {
+    prompt(MESSAGES['operationError'])
+    operation = readline.question();
+  }
+
+  return operation
 }
 
-let output;
-switch (operation) {
-  case '1':
-    output = Number(number1) + Number(number2);
-    break;
-  case '2':
-    output = Number(number1) - Number(number2);
-    break;
-  case '3':
-    output = Number(number1) * Number(number2);
-    break;
-  case '4':
-    output = Number(number1) / Number(number2);
-    break;
+function showResult(number1, number2, operation) {
+  let output;
+  switch (operation) {
+    case '1':
+      output = Number(number1) + Number(number2);
+      break;
+    case '2':
+      output = Number(number1) - Number(number2);
+      break;
+    case '3':
+      output = Number(number1) * Number(number2);
+      break;
+    case '4':
+      output = Number(number1) / Number(number2);
+      break;
+  }
+
+  prompt(`${MESSAGES['resultOutputPrefix']} ${output}`)
 }
 
-prompt(`The result is: ${output}`);
+function wantsToRestart() {
+  prompt(MESSAGES['restartRequest'])
+  let response = readline.question()
+  return response.toLowerCase() === 'y'
+}
+
+function startCalculator() {
+  let restart = false
+  do {
+    prompt("----------------------")
+    prompt(MESSAGES['welcome']);
+    let number1 = getFirstNumber()
+    let number2 = getSecondNumber()
+    let operation = getOperation()
+    showResult(number1, number2, operation)
+    restart = wantsToRestart()
+  } while (restart);
+  prompt(MESSAGES['goodBye'])
+}
+
+startCalculator()
