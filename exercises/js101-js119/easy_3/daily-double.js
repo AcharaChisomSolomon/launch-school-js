1916d49c263498809a15e830a820a8428e1d5eaf
AcharaChisomSolomon <ca0102c@gre.ac.uk>
2025-05-10 11:58:08 +0100

    practice using 'debugger'

--- exercises/js101-js119/easy_3/daily-double.js
+++ exercises/js101-js119/easy_3/daily-double.js
@@ -1,24 +1,33 @@
-function crunch(sentence) {
-  if (sentence.length === 0) return ''
+// function crunch(sentence) {
+//   if (sentence.length === 0) return ''
 
-  let startId = 0
-  let currId = 0
-  let output = ''
+//   let startId = 0
+//   let currId = 0
+//   let output = ''
 
-  while (currId < sentence.length) {
-    if (sentence[startId] != sentence[currId]) {
-      output += sentence[startId]
-      startId = currId
-    }
-    currId += 1
-  }
-  output += sentence[startId]
+//   while (currId < sentence.length) {
+//     if (sentence[startId] != sentence[currId]) {
+//       output += sentence[startId]
+//       startId = currId
+//     }
+//     currId += 1
+//   }
+//   output += sentence[startId]
 
-  return output
+//   return output
+// }
+
+// console.log(crunch('ddaaiillyy ddoouubbllee'));    // "daily double"
+// console.log(crunch('4444abcabccba'));              // "4abcabcba"
+// console.log(crunch('ggggggggggggggg'));            // "g"
+// console.log(crunch('a'));                          // "a"
+// console.log(crunch(''));                           // ""
+
+
+let i = 1;
+
+for (i = 0; i < 10; i += 1) {
+  console.log(i);
 }
 
-console.log(crunch('ddaaiillyy ddoouubbllee'));    // "daily double"
-console.log(crunch('4444abcabccba'));              // "4abcabcba"
-console.log(crunch('ggggggggggggggg'));            // "g"
-console.log(crunch('a'));                          // "a"
-console.log(crunch(''));                           // ""
+console.log(i);
